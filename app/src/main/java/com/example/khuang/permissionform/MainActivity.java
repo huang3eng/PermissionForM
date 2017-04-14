03cfa800187fdb094491bc3480cec4cafea3942e
x1ao3eng <qwe6542211>
2017-04-14 13:40:50 +0800

    使用PermissionsDispather添加Android6.0动态权限配置

--- app/src/main/java/com/example/khuang/permissionform/MainActivity.java
+++ app/src/main/java/com/example/khuang/permissionform/MainActivity.java
@@ -1,13 +1,99 @@
 package com.example.khuang.permissionform;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.OnNeverAskAgain;
+import permissions.dispatcher.OnPermissionDenied;
+import permissions.dispatcher.OnShowRationale;
+import permissions.dispatcher.PermissionRequest;
+import permissions.dispatcher.RuntimePermissions;
+
+@RuntimePermissions
 public class MainActivity extends AppCompatActivity {
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mButton = (Button) findViewById(R.id.btn);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // MainActivityPermissionsDispatcher这个类要先在Build中makeProject或者makeModule才会生成。
+                MainActivityPermissionsDispatcher.showCameraWithCheck(MainActivity.this);
+            }
+        });
+
+
+    }
+    @NeedsPermission(Manifest.permission.CAMERA)
+    void showCamera(){
+        Intent intent = new Intent(); //调用照相机
+        intent.setAction("android.media.action.STILL_IMAGE_CAMERA");
+        startActivity(intent);
+    }
+
+    /**
+     * 这个API主要用于给用户一个申请权限的解释，该方法只有在用户在上一次已经拒绝过你的这个权限申请。
+     * 也就是说，用户已经拒绝一次了，你又弹个授权框，你需要给用户一个解释，为什么要授权，则使用该方法。
+     * @param request
+     */
+    @OnShowRationale(Manifest.permission.CAMERA)
+    void showRationaleForCamera(final PermissionRequest request) {
+        new AlertDialog.Builder(this)
+                .setMessage("拒绝该权限，将不能使用照相机功能")
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        request.proceed();
+                    }
+                })
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        request.cancel();
+                    }
+                })
+                .show();
+    }
+
+    /**
+     * 用户拒绝该权限执行的方法
+     */
+    @OnPermissionDenied(Manifest.permission.CAMERA)
+    void showDeniedForCamera() {
+        Toast.makeText(this, "OnPermissionDenied", Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * 用户拒绝该权限，并且选中不在询问执行的方法
+     */
+    @OnNeverAskAgain(Manifest.permission.CAMERA)
+    void showNeverAskForCamera() {
+        Toast.makeText(this, "OnNeverAskAgain", Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * 权限请求回调，提示用户之后，用户点击“允许”或者“拒绝”之后调用此方法
+     * 重写该方法，交给MainActivityPermissionsDispatcher处理
+     * @param requestCode  定义的权限编码
+     * @param permissions 权限名称
+     * @param grantResults 允许/拒绝
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        MainActivityPermissionsDispatcher.onRequestPermissionsResult(MainActivity.this,requestCode,grantResults);
     }
 }
